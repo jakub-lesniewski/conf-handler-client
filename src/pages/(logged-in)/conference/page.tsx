aba4fd4b62a20befe519890c2282ca4c71e1ea06
Jakub Leśniewski <jakubjanlesniewski@gmail.com>
2024-04-23 13:00:55 +0200

    added date changing logic

--- src/pages/(logged-in)/conference/page.tsx
+++ src/pages/(logged-in)/conference/page.tsx
@@ -1,35 +1,57 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import Timeline from "./timeline";
+import { useState } from "react";
+import { Card, CardContent, CardHeader, CardFooter } from "@/components/ui/card";
 import { useLoaderData } from "react-router-dom";
 import { TimelineElement } from "./conference-types";
-import { useState } from "react";
+import { MoveLeft, MoveRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export default function Conference() {
-  const [currDate, setCurrDate] = useState({
-    currDayOfWeek: new Date().toLocaleString("default", { weekday: "long" }),
-    currDay: new Date().getDate(),
-    currMonth: new Date().toLocaleString("default", { month: "long" }),
-    currYear: new Date().getFullYear(),
-  });
+  const [currDate, setCurrDate] = useState<Date>(new Date());
 
   const timeline = useLoaderData() as TimelineElement[];
 
-  console.log(timeline);
-  console.log(currDate);
+  function handleSetNextDay() {
+    const nextDay = new Date(currDate);
+    nextDay.setDate(nextDay.getDate() + 1);
+    console.log(nextDay);
+
+    setCurrDate(nextDay);
+  }
+
+  function handleSetPrevDay() {
+    const prevDay = new Date(currDate);
+    prevDay.setDate(prevDay.getDate() - 1);
+    console.log(prevDay);
+
+    setCurrDate(prevDay);
+  }
 
   return (
-    <Card className="my-8 flex flex-col justify-between w-[350px]">
-      <div>
-        <CardHeader className="border-b">
-          <p className="font-bold tracking-widest">{currDate.currDayOfWeek}</p>
-          <p className="text-sm">
-            {currDate.currDay} {currDate.currMonth}, {currDate.currYear}
-          </p>
-        </CardHeader>
+    <Card className="my-8 flex flex-col justify-between w-[350px] h-[500px]">
+      <div className="flex flex-col justify-between h-full">
+        <div>
+          <CardHeader className="border-b">
+            <p className="font-bold tracking-widest">{currDate.toLocaleDateString("en-UK", { weekday: "long" })}</p>
+            <p className="text-sm">
+              {currDate.getDate()} {currDate.toLocaleDateString("en-UK", { month: "long" })}, {currDate.getFullYear()}
+            </p>
+          </CardHeader>
 
-        <CardContent className="w-full">
-          <Timeline timeline={timeline} />
-        </CardContent>
+          <CardContent className="w-full flex-1">
+            <Timeline timeline={timeline} />
+          </CardContent>
+        </div>
+
+        <CardFooter className="border-t p-1 flex justify-around">
+          <Button onClick={handleSetPrevDay} variant="ghost">
+            <MoveLeft strokeWidth={1} className="mr-2" /> previous day
+          </Button>
+          <Button onClick={handleSetNextDay} variant="ghost">
+            next day
+            <MoveRight strokeWidth={1} className="ml-2" />
+          </Button>
+        </CardFooter>
       </div>
     </Card>
   );
