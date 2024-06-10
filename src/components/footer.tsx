249a3f874446ad9467903ce83212940f5ddca2d7
ayroh <jakubjanlesniewski@gmail.com>
2024-06-11 00:04:20 +0200

    fixed footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="flex flex-col items-start gap-1 bg-primary px-4 py-2 text-white">
       <h4 className="font-semibold tracking-tight">Need a hand?</h4>
-      <div className="flex w-full justify-between">
+      <div className="flex w-full justify-between md:w-[350px]">
         <div className="flex flex-col">
           <p>email: </p>
           <p>phone:</p>
