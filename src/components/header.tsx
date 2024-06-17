e32afd4aadc14b34ee0794125d154c0d4a4726c8
ayroh <jakubjanlesniewski@gmail.com>
2024-06-18 00:25:44 +0200

    minor refactoring

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,13 @@
 import { useAuth } from "@/lib/AuthContext";
-
+import { ThemeToggle } from "./theme-toggler";
 import Sidebar from "./sidebar";
 import MessageWarning from "./message-warning";
-import { ThemeToggle } from "./theme-toggler";
 
 export default function Header() {
   const { loggedUser } = useAuth();
 
   return (
-    <header className="flex h-[--header-height] w-full justify-between bg-primary p-3">
+    <header className="flex w-full items-center justify-between bg-primary px-4 py-2">
       {loggedUser && <MessageWarning />}
       <div className={`flex items-center gap-2 ${loggedUser ? "" : "ml-auto"}`}>
         <ThemeToggle />
