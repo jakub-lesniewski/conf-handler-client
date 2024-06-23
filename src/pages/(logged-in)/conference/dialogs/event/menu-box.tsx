54b62c7a3b9e04469f96dd0d7f0de475642478c1
ayroh <jakubjanlesniewski@gmail.com>
2024-06-23 14:49:47 +0200

    destructured menu box

--- src/pages/(logged-in)/conference/dialogs/event/menu-box.tsx
+++ src/pages/(logged-in)/conference/dialogs/event/menu-box.tsx
@@ -5,11 +5,13 @@ type MenuBoxProps = {
 };
 
 export default function MenuBox({ menu }: MenuBoxProps) {
+  const { header, menuItems } = menu;
+
   return (
     <div className="border-b">
-      <h1 className="border-b pb-1 font-semibold">{menu.header}</h1>
+      <h1 className="border-b pb-1 font-semibold">{header}</h1>
       <ul className="ml-1 max-h-[60svh] list-disc space-y-1 overflow-auto">
-        {menu.menuItems.map((item, index) => (
+        {menuItems.map((item, index) => (
           <li key={index}>- {item}</li>
         ))}
       </ul>
